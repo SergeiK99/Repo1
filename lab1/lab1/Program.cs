86e09962f2a0d392ff9eff630bcc8d2371970b79
SergeiK99 <skx2013@mail.ru>
2022-10-12 19:34:54 +0700

    Добавлен код

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a%4==0)
+            {
+                Console.WriteLine("Год високосный");
+
+            }
+            else
+            {
+                Console.WriteLine("Год не високосный");
+            }
+        }
+    }
+}
